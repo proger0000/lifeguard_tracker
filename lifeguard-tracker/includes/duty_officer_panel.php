3896d3e354c5b331c433751da429a3e694b73f21
proger0000 <47700398+proger0000@users.noreply.github.com>
2025-05-23 11:10:34 +0300

    Delete lifeguard-tracker directory

--- lifeguard-tracker/includes/duty_officer_panel.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_role('duty_officer'); // Залишаємо перевірку ролі для прямого доступу Чергового
-global $pdo; // Передаємо PDO, якщо потрібно
-// Включаємо відокремлений контент
-require_once __DIR__ . '/panels/duty_officer_content.php';
-?>
